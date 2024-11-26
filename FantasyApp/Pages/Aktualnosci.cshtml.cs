74af250ddf3995206d5d271636157f13120c761f
Adam Wujkiewicz <adamwwujkiewicz@gmail.com>
2024-11-26 19:23:32 +0100

    Add pages
    Added pages:
    -Aktualności
    -Budowanie zespołu
    -Kontakt
    -Rankingi
    -Statystyki
    -Terminarz
    -Zasady/FAQ

---
+++ FantasyApp/Pages/Aktualnosci.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FantasyApp.Pages
+{
+    public class AktualnosciModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ FantasyApp/Pages/Kontakt.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FantasyApp.Pages
+{
+    public class KontaktModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
