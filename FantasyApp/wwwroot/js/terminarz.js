7a49de61645acdbb66ac05df969bc28ddf702380
Pawe� Tomczak <91501@office365.ukw.edu.pl>
2024-12-08 18:40:37 +0100

    Dodanie terminarz.js

---
+++ FantasyApp/wwwroot/js/terminarz.js
@@ -0,0 +1,14 @@
+﻿function showContent() {
+    // Ukryj wszystkie pola
+    document.querySelectorAll('.content').forEach(function (div) {
+        div.style.display = 'none';
+    });
+
+// Pobierz wybraną opcję
+const selectedValue = document.getElementById('select-option').value;
+
+// Pokaż odpowiednie pole
+if (selectedValue !== "none") {
+    document.getElementById(selectedValue).style.display = 'block';
+            }
+        }
