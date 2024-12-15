6fb84bb49d7d8e832a7a3d6e79d40e9be46525c1
Łukasz Jurek <80386285+TaktyKK1@users.noreply.github.com>
2024-12-15 18:03:40 +0100

    Utworzenie plików css i js

---
+++ FantasyApp/wwwroot/js/ranking.js
@@ -0,0 +1,19 @@
+﻿    function showContent() {
+        // Ukryj wszystkie pola
+        document.querySelectorAll('.content').forEach(function (div) {
+            div.style.display = 'none';
+        });
+
+    // Pobierz wybraną opcję
+    const selectedValue = document.getElementById('select-option').value;
+
+    // Zmień nagłówek i pokaż odpowiednie pole
+    const mainHeading = document.getElementById('main-heading');
+    if (selectedValue === "ranking-ogolny") {
+        mainHeading.textContent = "Ranking ogólny";
+    document.getElementById('ranking-ogolny').style.display = 'block';
+            } else if (selectedValue === "ligi-prywatne") {
+        mainHeading.textContent = "Ranking lig prywatnych";
+    document.getElementById('ligi-prywatne').style.display = 'block';
+            }
+        }
