0a67511f41c7fe5ae319981688411d3bbc0371c1
Adam Wujkiewicz <adamwwujkiewicz@gmail.com>
2024-12-16 16:39:54 +0100

    Dodano css i js
    Dodano pliki css i js i wydzielono w nich odpowiednie fragmenty kodu

---
+++ FantasyApp/wwwroot/js/budowanie_zespolu.js
@@ -0,0 +1,16 @@
+﻿function showSection(sectionId) {
+    // Ukrywamy wszystkie sekcje
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => section.classList.remove('active'));
+
+    // Deaktywujemy przyciski
+    const buttons = document.querySelectorAll('.nav-button');
+    buttons.forEach(button => button.classList.remove('active'));
+
+    // Wyświetlamy odpowiednią sekcję
+    document.getElementById(sectionId).classList.add('active');
+
+    // Aktywujemy odpowiedni przycisk
+    const activeButton = Array.from(buttons).find(button => button.onclick.toString().includes(sectionId));
+    if (activeButton) activeButton.classList.add('active');
+}
