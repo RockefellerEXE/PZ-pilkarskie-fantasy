161ea59eb52f07afd75e168de3e76d47268a849c
Adam Wujkiewicz <adamwwujkiewicz@gmail.com>
2024-12-17 18:31:34 +0100

    Add Database
    - Utworzono klasy: Klub, Mecz, StatystykiZawodnika, Uzytkownik, Zawodnik.
    - utworzono init migration
    - dodano connection string
    - utworzono dbcontext
    Dodano Entity Framework Core w wersji 6.0.27

---
+++ FantasyApp/DAL/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FantasyApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FantasyApp.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Zawodnik> Zawodnicy { get; set; }
+        public DbSet<Klub> Kluby { get; set; }
+        public DbSet<StatystykiZawodnika> StatystykiZawodnikow { get; set; }
+        public DbSet<Uzytkownik> Uzytkownicy { get; set; }
+        public DbSet<Mecz> Mecze { get; set; }
+
+    }
+}
