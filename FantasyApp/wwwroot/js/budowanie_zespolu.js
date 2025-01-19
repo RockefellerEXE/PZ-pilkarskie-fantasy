01294be3e2e9a01f102649236056bbdfed554d74
Łukasz Jurek <80386285+TaktyKK1@users.noreply.github.com>
2025-01-19 12:36:43 +0100

    Ustawienie drużyny w budowaniu zespołu, przełączanie na odpowiednią sekcję

--- FantasyApp/wwwroot/js/budowanie_zespolu.js
+++ FantasyApp/wwwroot/js/budowanie_zespolu.js
@@ -1,4 +1,30 @@
-﻿// Ensure this script is included in the layout
+﻿    document.addEventListener('DOMContentLoaded', function () {
+        const sections = document.querySelectorAll('.content-section');
+    const sectionId = window.location.hash.substring(1); // Pobierz fragment z URL
+    console.log(sectionId)
+    // Domyślnie pokaż sekcję "team", jeśli nie ma hash w URL
+    let activeSectionId = sectionId || 'team';
+
+        // Usuń klasę ukrywającą po załadowaniu strony
+        sections.forEach(section => {
+            if (section.id === activeSectionId) {
+        section.classList.add('active'); // Ustaw aktywną sekcję
+            } else {
+        section.classList.remove('active');
+            }
+    section.classList.remove('hidden-before-load'); // Usuń ukrywanie
+        });
+
+    // Scroll do sekcji, jeśli fragment jest w URL
+        if (sectionId) {
+            history.replaceState(null, null, ' ');
+            const section = document.getElementById(sectionId);
+            if (section) {
+                showSection(sectionId); // Ustaw aktywną sekcję
+            }
+        }
+    });
+
 
 function showSection(sectionId) {
     const sections = document.querySelectorAll('.content-section');
@@ -20,5 +46,3 @@ function showSection(sectionId) {
         }
     });
 }
-
-// Additional JavaScript logic can be added here as needed
